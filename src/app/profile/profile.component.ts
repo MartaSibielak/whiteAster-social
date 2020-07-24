832a9b8c761e6f031e1cf373934c8ef566b464d1
propertyofkeow <marta.sibielak92@gmail.com>
2020-07-24 23:05:17 +0200

    make sidebar opeen and close functionality

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  @ViewChild('wrapper')
+  private wrapper: ElementRef;
+
+  open = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openSideBar(){
+    if (this.open === false){
+      this.open = true;
+      this.wrapper.nativeElement.classList.add('section-wrapper');
+    }else if (this.open === true){
+      this.open = false;
+      this.wrapper.nativeElement.classList.remove('section-wrapper');
+    }
   }
 
 }
