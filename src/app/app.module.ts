5624b70086742741b8c211260e5f25c237dee126
propertyofkeow <marta.sibielak92@gmail.com>
2020-07-24 21:03:14 +0200

    create sidebar and style it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { MessagesComponent } from './messages/messages.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    MessagesComponent
+    MessagesComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
